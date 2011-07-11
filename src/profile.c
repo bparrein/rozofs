608686339eccbf6e0d019b7126d2979b58b794a0
adrenaline <sylvain.david@fizians.org>
2011-07-11 15:50:39 +0200

    Fixed some bugs in file 'profile.c'.

--- src/profile.c
+++ src/profile.c
@@ -18,6 +18,8 @@
  */
 
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
 #include "log.h"
 #include "profile.h"
 
@@ -45,8 +47,6 @@ struct timeval *result, *x, *y;
     return x->tv_sec < y->tv_sec;
 }
 
-
-
 int timeval_addto(timeval * dest, timeval * toadd) {
     int usec_add = 0;
     if ((usec_add = dest->tv_usec + toadd->tv_usec) > 1000000) {
@@ -61,47 +61,45 @@ int timeval_addto(timeval * dest, timeval * toadd) {
 
 int save_profile_result(char *file_name) {
 
-    int fd;
+    FILE * file;
 
     // Open the output file
-    if ((fd =
-         open(file_name, O_RDWR | O_CREAT,
-              S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO)) < 0) {
+    if ((file =
+         fopen(file_name,"rw")) < 0) {
         perror("open failed");
         return -1;
     }
 
-    fprintf(fd, "time spent in export exchanges : %ld sec, %ld usec\n",
+    fprintf(file, "time spent in export exchanges: %ld sec, %ld usec\n",
             profile_time[1].tv_sec, profile_time[1].tv_usec);
-    fprintf(fd, "time spent in storage exchanges : %ld sec, %ld usec\n",
+    fprintf(file, "time spent in storage exchanges: %ld sec, %ld usec\n",
             profile_time[0].tv_sec, profile_time[0].tv_usec);
-    fprintf(fd, "time spent in transform forward : %ld sec, %ld usec\n",
+    fprintf(file, "time spent in transform forward: %ld sec, %ld usec\n",
             profile_time[2].tv_sec, profile_time[2].tv_usec);
-    fprintf(fd, "time spent in transform inverse : %ld sec, %ld usec\n",
+    fprintf(file, "time spent in transform inverse: %ld sec, %ld usec\n",
             profile_time[3].tv_sec, profile_time[3].tv_usec);
 
-    close(fd);
+    close(file);
 
     return 0;
 }
 
 void log_profile() {
-    info("time spent in export exchanges : %ld sec, %ld usec\n",
-         profile_time[1].tv_sec, profile_time[1].tv_usec);
-    info("time spent in storage exchanges : %ld sec, %ld usec\n",
-         profile_time[0].tv_sec, profile_time[0].tv_usec);
-    info("time spent in transform forward : %ld sec, %ld usec\n",
-         profile_time[2].tv_sec, profile_time[2].tv_usec);
-    info("time spent in transform inverse : %ld sec, %ld usec\n",
-         profile_time[3].tv_sec, profile_time[3].tv_usec);
+    info("time spent in export exchanges: %ld sec, %ld usec\n",
+            profile_time[1].tv_sec, profile_time[1].tv_usec);
+    info("time spent in storage exchanges: %ld sec, %ld usec\n",
+            profile_time[0].tv_sec, profile_time[0].tv_usec);
+    info("time spent in transform forward: %ld sec, %ld usec\n",
+            profile_time[2].tv_sec, profile_time[2].tv_usec);
+    info("time spent in transform inverse: %ld sec, %ld usec\n",
+            profile_time[3].tv_sec, profile_time[3].tv_usec);
 }
 
-
 void log_storage_profile() {
-    info("time spent in managing incoming requests : %ld sec, %ld usec\n",
-         profile_time[1].tv_sec, profile_time[1].tv_usec);
-    info("time spent in execution of the requests : %ld sec, %ld usec\n",
-         profile_time[0].tv_sec, profile_time[0].tv_usec);
-    info("time spent in sending the result back : %ld sec, %ld usec\n",
-         profile_time[2].tv_sec, profile_time[2].tv_usec);
+    info("time spent in managing incoming requests: %ld sec, %ld usec\n",
+            profile_time[1].tv_sec, profile_time[1].tv_usec);
+    info("time spent in execution of the requests: %ld sec, %ld usec\n",
+            profile_time[0].tv_sec, profile_time[0].tv_usec);
+    info("time spent in sending the result back: %ld sec, %ld usec\n",
+            profile_time[2].tv_sec, profile_time[2].tv_usec);
 }
